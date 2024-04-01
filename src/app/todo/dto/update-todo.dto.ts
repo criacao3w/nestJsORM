e6216a4a7283e9a08861633de644f349d04eac58
Roberto Ancelmo <criacao3w@gmail.com>
2024-04-01 18:06:38 -0300

    DTO Validator

--- src/app/todo/dto/update-todo.dto.ts
+++ src/app/todo/dto/update-todo.dto.ts
@@ -1,10 +1,4 @@
 import { IsNotEmpty } from "class-validator";
 import { CreateTodoDto } from "./create-todo.dto";
 
-export class UpdateTodoDto extends CreateTodoDto {
-    @IsNotEmpty()
-    task: string;
-
-    @IsNotEmpty()
-    isDone: number;
-}
+export class UpdateTodoDto extends CreateTodoDto {}
