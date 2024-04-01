58790dff952f1bf734111da626effbb019b772e8
Roberto Ancelmo <criacao3w@gmail.com>
2024-04-01 14:37:10 -0300

    Type Return in methods to service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -11,11 +11,11 @@ export class TodoService {
         private readonly todoRepository: Repository<TodoEntity>,
     ){} 
 
-    async findAll() {
+    async findAll(): Promise<TodoEntity[] | null>{
         return await this.todoRepository.find();
     }
 
-    async findOne(id: number) {
+    async findOne(id: number): Promise<TodoEntity | null> {
         try {
             return await this.todoRepository.findOneBy({
                 id: id
@@ -25,20 +25,19 @@ export class TodoService {
         }
     }
 
-    async create(data) {
+    async create(data): Promise<TodoEntity[]> {
         return await this.todoRepository.save(this.todoRepository.create(data));
     }
 
-    async update(id: number, data) {
+    async update(id: number, data): Promise<TodoEntity | null> {
         const todo = await this.todoRepository.findOne({where: {id: Number(id)}});
 
         this.todoRepository.merge(todo, data);
         return await this.todoRepository.save(todo);
     }
 
-    async remove(id: number) {
+    async remove(id: number): Promise<void> {
         await this.todoRepository.findOne({where: {id: Number(id)}});
-
         await this.todoRepository.softDelete(id)
     }
 }
